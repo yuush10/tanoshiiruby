b5a45cec7c849e6591f24dc4fb7d08ddb5e89344
Yu Ushio <yu@pretia.co.jp>
2016-02-01 15:46:12 +0900

    Remove 1-3 and 4-11

--- 1-3/use_grep.rb
+++
@@ -1,5 +0,0 @@
-require "./grep" #grep.rbの読み込み
-
-pattern = Regexp.new(ARGV[0])
-filename = ARGV[1]
-simple_grep(pattern, filename) #simple_grepメソッドの起動
--- 4-11/strip.rb
+++
@@ -1,6 +0,0 @@
-file = File.open(ARGV[0])
-file.each_line do |line|
-  next if /^\s*$/ =~ line #空白行
-  next if /^#/ =~ line #シャープで始まる行
-  puts line
-end
